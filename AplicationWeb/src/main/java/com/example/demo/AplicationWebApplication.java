14c942121f45e8428b7bbeffa080435bc36c86d3
irvin ulises luna balderas <iluna@ids.com.mx>
2019-09-30 22:08:12 -0500

    primer commit inicial desde spring

---
+++ AplicationWeb/src/main/java/com/example/demo/AplicationWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicationWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicationWebApplication.class, args);
+	}
+
+}
